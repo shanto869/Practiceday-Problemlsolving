eedd9136e30693fc9603c7ae944a96ee3402201c
shanto869 <shantobiswas869@gmail.com>
2022-08-10 07:47:10 +0600

    prime solution added

--- array.js
+++ array.js
@@ -12,7 +12,7 @@ function oddEvenSum(arr){
     for(let i = 0; i < arr.length; i++){
         let number = arr[i];
         totalSum = totalSum + number
-        
+
         if(number % 2 === 0){
             evenArr.push(number);
             evenSum = evenSum + number;
@@ -30,3 +30,67 @@ function oddEvenSum(arr){
 }
 
 oddEvenSum(oddEvenArr);
+
+// ====================================
+
+//  remove the duplicate element form an array
+
+const myArray = [2, 1, 4, 8, 6, 4, 1, 3, 8, 5, 3, 1, 8, 9, 4, 2, 7, 4, 5, 8, 0, 9];
+
+function removeDuplicate(numbers){
+    let uniqueArr = [];
+    const sortArr = uniqueArr.sort();
+
+    for(let i = 0; i < numbers.length; i++){
+        let num = numbers[i];
+
+        if(uniqueArr.includes(num) === false){
+             uniqueArr.push(num);
+        }
+    }
+    console.log(uniqueArr);
+    console.log(sortArr);
+}
+
+removeDuplicate(myArray);
+// ========================================
+
+// find out the maximum number from an array
+
+const numbers = [9, 8, 4, 5, 2, 5, 20, 48, 45, 938, 4, 43];
+
+function maxNumber(numbers){
+    let maxNum = numbers[0];
+
+    for(let i = 0; i < numbers.length; i++){
+        let number = numbers[i];
+
+        if(maxNum < number){
+            maxNum = number;
+        }
+    }
+    console.log(maxNum);
+}
+
+maxNumber(numbers);
+// ===================================
+
+// calculate the sum of odd numbers getter than 10 and less than 50;
+const numbersArr = [1, 2, 3, 4, 5, 6, 7,8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39,40, 41, 42, 43, 44, 45, 46, 47, 49, 50];
+
+function sumOfOddNumbers(numbers){
+    let oddSum = 0;
+    
+    for(let i = 9; i < numbers.length; i++){
+        const number = numbers[i];
+
+        if(number % 2 !== 0){
+            // console.log(number)
+            oddSum = oddSum + number;
+        }
+    }
+    console.log('Total sum of odd number is:', oddSum);
+}
+
+sumOfOddNumbers(numbersArr);
+// ===============================
---
+++ practice-3.js
@@ -0,0 +1,75 @@
+// will check the number is positive or negative.
+
+function isPositive(number){
+    if(number > 0){
+        console.log(number, 'this number is positive')
+    }
+    else{
+        console.log(number, 'this number is negative')
+    }
+}
+
+isPositive(6);
+
+// ==================================
+// fing the area of triangle
+
+function triangleArea(base, heigth){
+    const area = (1 / 2) * base * heigth;
+    console.log('The area of triangel is:', area);
+}
+
+triangleArea(5, 10);
+// ========================================
+// find the area of rectangle
+
+function rectangleArea(heigth, width){
+    const area = heigth * width;
+    console.log('The area of rectangle is:', area);
+}
+
+rectangleArea(5, 8);
+// =========================================
+// check the number is prime or not
+
+// function isPrime(number){
+//     if(number < 1){
+//         console.log(`${number} is not a Prime Number`);
+//     }
+//     else if(number === 1){
+//         console.log(`${number} is neither prime nor composite number`);
+//     }
+//     else{
+//         for(let i = 2; i < number; i++){
+//             if(number % i === 0){
+//                 var result = (`${number} is not a prime number`);
+//                 break;
+//             }
+//             else{
+//                 var result = (`${number} is a prime number`)
+//             }
+//         }
+//         console.log(result);
+//     }
+// }
+
+// isPrime(29)
+function isPrime(num){
+    if(num < 1){
+        return `${num} this number is not a prime number`;
+    }
+    else if(num === 1){
+        return `${num} is neither  prime nor composite`;
+    }
+    else{
+        for(let i = 2; i < num; i++){
+            if(num % i === 0){
+                return `${num} is not a prime number`;
+            }
+        }
+        return `${num} is a prime number`;
+    }
+}
+
+const checkPrime = isPrime(11);
+console.log(checkPrime);
